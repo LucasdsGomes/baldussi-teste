63d7c3963e406c906dc3cf7d11a89b2c4bc0b2ce
Lucas Gomes <programmingpyrubygo@gmail.com>
2025-08-29 23:37:49 -0300

    feat(front): adding dashboard users route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginHome from './components/LoginHome.tsx'
 import Register from './components/Register.tsx'
 import Dashboard from './components/Dashboard.tsx'
+import DashboardUsers from './components/DashboardUsers.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginHome />} />
+        <Route path="/dashboard-users" element={<DashboardUsers />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
